f4735bdc608fce7bc5528dd424d780f00b5166d6
kasstechweb <kasstechweb@gmail.com>
2022-12-11 20:31:29 +0200

    upload statement view and basic python

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -3,12 +3,18 @@
 from django.urls import path, re_path
 from apps.home import views
 
+# from .views import (
+#     upload_statement,
+# )
+
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
 
     # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
+    # re_path(r'^.*\.*', views.pages, name='pages'),
+    path('upload_statement/', views.upload_statement, name='upload_statement'),
 
+    # path('upload_statement/', views.upload_statement, name='upload_statement'),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 
+# from django.shortcuts import render  
+# from .forms import StudentForm  
+
 
 @login_required(login_url="/login/")
 def index(request):
@@ -31,6 +34,27 @@ def pages(request):
         html_template = loader.get_template('home/' + load_template)
         return HttpResponse(html_template.render(context, request))
 
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template('home/page-404.html')
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+        html_template = loader.get_template('home/page-500.html')
+        return HttpResponse(html_template.render(context, request))
+
+# Upload statement page and post 
+@login_required(login_url="/login/")
+def upload_statement(request):
+    context = {}
+    if request.method == "POST":
+        # print(request.POST.get('test'))
+        print(request)
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+        html_template = loader.get_template('home/upload_statement.html' )
+        return HttpResponse(html_template.render(context, request))
     except template.TemplateDoesNotExist:
 
         html_template = loader.get_template('home/page-404.html')
